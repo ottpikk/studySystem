627f0e51b8933dc388ac8dc112f9268d4fa8c073
Ott Pikk <ott@karmai.eu>
2022-08-13 10:59:34 +0300

    add necessary models

---
+++ src/main/java/com/sda/studysystem/models/Login.java
@@ -0,0 +1,14 @@
+package com.sda.studysystem.models;
+
+import lombok.Data;
+
+/**
+ * Login model
+ *
+ * @author Ott Pikk
+ */
+@Data
+public class Login {
+    private String username;
+    private String password;
+}
